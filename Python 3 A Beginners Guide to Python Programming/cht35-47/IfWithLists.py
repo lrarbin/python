60e1f1104d27f5311a6d014a68683aa3f2fc66bf
Laurence Arbin <lrarbin@outlook.com>
2022-08-21 11:41:31 +0100

    Update 21_08_2022

---
+++ Python 3 A Beginners Guide to Python Programming/cht35-47/IfWithLists.py
@@ -0,0 +1,8 @@
+shopping_cart = ['pens','pencils']
+
+if shopping_cart:
+    for item in shopping_cart:
+        print("Adding " + item + " to your cart.")
+    print("Your order is complete")
+else:
+    print("You must select an item before an item before proceeding.")
---
+++ Python 3 A Beginners Guide to Python Programming/cht48-56/Dictionary1.py
@@ -0,0 +1,5 @@
+
+book_1 = {'author' : 'tony staunton', 'price' : 10}
+
+print(book_1['author'])
+print(book_1['price'])
